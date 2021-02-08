ab180925e3047b8baac0e4d6f9be111c6c4d7f64
Yousuf Abdullatif <2482572a@student.gla.ac.uk>
2021-02-08 22:54:56 +0100

    Chapter 4 exercises done.

---
+++ rango/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 19:10:09 2021
+
+@author: yousu
+"""
+
+from django.urls import path
+from rango import views
+app_name = 'rango'
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('about/',views.about, name = 'about'),
+    ]
